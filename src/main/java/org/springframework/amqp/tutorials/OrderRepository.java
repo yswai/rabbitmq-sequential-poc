a2e46a5d717f860c4b6e773a2d0dac306fae737e
ys_wai <yswai1986@gmail.com>
2021-10-22 00:40:48 +0800

    init

---
+++ src/main/java/org/springframework/amqp/tutorials/OrderRepository.java
@@ -0,0 +1,9 @@
+package org.springframework.amqp.tutorials;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends PagingAndSortingRepository<Order, Long> {
+
+}
---
+++ src/main/java/org/springframework/amqp/tutorials/ProductRepository.java
@@ -0,0 +1,13 @@
+package org.springframework.amqp.tutorials;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends PagingAndSortingRepository<Product, Long> {
+
+  Optional<Product> findByName(String name);
+
+}
